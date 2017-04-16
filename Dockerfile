ca165df9b7ddcb1a349806001d18667123d5651a
eric.zhang <84089842@qq.com>
2017-04-17 00:41:37 +0800

    add install git

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+FROM centos:7
+MAINTAINER Eric <eric.zhang@famesmart.com>
+
+#install npm cnpm
+RUN  curl -sL -o /etc/yum.repos.d/khara-nodejs.repo https://copr.fedoraproject.org/coprs/khara/nodejs/repo/epel-7/khara-nodejs-epel-7.repo && \
+    yum install -y nodejs nodejs-npm && \
+    npm install cnpm -g --registry=https://registry.npm.taobao.org && \
+
+#antd-admin
+    yum install git && \
+    git clone https://github.com/zuiidea/antd-admin.git /tmp/antd-admin && \
+    cd /tmp/antd-admin
+    cnpm i  && \
+    cnpm install jsonp  && \
+    cd /tmp
+    rm -rf -y antd-admin && \
+
+
+#Create dist folder
+VOLUME ["/data/www"]
+
+#Start
+ADD start.sh /
+RUN chmod +x /start.sh
+
+#Start it
+ENTRYPOINT ["/start.sh"]
+
+#Start web server
+#CMD ["/bin/bash", "/start.sh"]
---
+++ start.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#########################################################################
+# File Name: start.sh
+# Author: Eric_zhang
+# Email:  84089842@qq.net
+# Version:
+# Created Time: 2017/4/3
+#########################################################################
+
+DATA_DIR=/data/www
+
+set -e
+
+    if [ -z "$BRANCH" ]; then
+            echo >&2 'error:  missing BRANCH'
+            echo >&2 '  Did you forget to add -e BRANCH=... ?'
+            exit 1
+    fi
+
+    if [ -z "$DIST_DIR" ]; then
+         echo >&2 'error:  missing DIST_DIR'
+         echo >&2 '  Did you forget to add -e DIST_DIR=... ?'
+         exit 1
+     fi
+
+     deploy_path="/tmp/community-backend"
+     project_path="git@git.famesmart.com:Jully/community-backend.git -b "$BRANCH
+
+     if [  -d "$deploy_path" ]
+        rm -rf $deploy_path
+     fi
+    git clone $project_path $deploy_path
+    cd $deploy_path
+    cnpm  i 
+    cnpm install jsonp
+    npm run lint
+    cp -r dist $DATA_DIR$DIST_DIR
+
+
+
